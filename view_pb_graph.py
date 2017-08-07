569a1611b222a8e87c3a2160feab1b522cf0383e
simonhkswan <shks2@cam.ac.uk>
2017-08-07 06:53:22 +0100

    added originial optimised graph.

--- view_pb_graph.py
+++ view_pb_graph.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
+import time
 
 # Change the two file paths
-pb_file_loc = 'graphs/mnist_optimised_graph.pb' ##PB FILE PATH##
-log_file_loc = 'graphs/optimised' ##LOG OUTPUT PATH FOR TENSOR BOARD##
+pb_file_loc = 'graphs/mnist_working_graph.pb' ##PB FILE PATH##
+log_file_loc = 'graphs/mnistworking' ##LOG OUTPUT PATH FOR TENSOR BOARD##
 
 fw = tf.summary.FileWriter(log_file_loc)
 from tensorflow.python.platform import gfile
@@ -12,5 +13,7 @@ with tf.Session() as sess:
 		gdef.ParseFromString(infile.read())
 		g_in = tf.import_graph_def(gdef)
 		fw.add_graph(sess.graph)
+time.sleep(5)
+print("Output file saved.")
 
 # Then run tensorboard --logdir='./' in the log_file location to view the graph.
